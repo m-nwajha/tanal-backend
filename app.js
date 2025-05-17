d5e6c7af43298d36f30530a3e8ef7aa90de5d81c
Mohamed Alnawjha <m.nawjha@gmail.com>
2025-05-17 16:56:21 +0300

    .

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const apiRoutes = require("./routes/apiRoutes");
-
-app.use("/api", apiRoutes);
-
-module.exports = app;
--- controllers/helloController.js
+++
@@ -1,3 +0,0 @@
-exports.sayHello = (req, res) => {
-  res.json({ message: "Hello from modular Express inside Netlify Functions!" });
-};
