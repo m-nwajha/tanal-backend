896bab3df4d4ed3d7c99b279e8637990dba328cd
Mohamed Alnawjha <m.nawjha@gmail.com>
2025-05-17 16:36:11 +0300

    use ES6 module.

--- netlify/functions/express-server.js
+++ netlify/functions/express-server.js
@@ -1,4 +1,4 @@
-const serverless = require("serverless-http");
-const app = require("../../app");
+import serverless from 'serverless-http';
+import app from '../../src/app.js';
 
-module.exports.handler = serverless(app);
+export const handler = serverless(app);
